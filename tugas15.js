f935e3b1468d046e6a583166faf1395ff70641de
Andika Andriana <andriana.mia1@gmail.com>
2019-12-07 16:38:15 +0700

    Tugas 15 Javascript
    Tugas ini Saya kumpulkan untuk Niomic.

---
+++ tugas15.js
@@ -0,0 +1,13 @@
+const tugas15 = () => {
+  var alas = 8;
+  var tinggi = 6;
+
+  console.log("Nilai AB : ", alas);
+  console.log("Nilai BC : ", tinggi);
+
+  sisiMiring = Math.sqrt(Math.pow(alas, 2) + Math.pow(tinggi, 2));
+
+  console.log("Panjang sisi AC pada segitiga siku-siku tersebut adalah : ", sisiMiring, " cm");
+}
+
+tugas15();
